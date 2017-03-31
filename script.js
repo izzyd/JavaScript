fd9fabefb8d6ffa39e3e6db3c9af9c906473ae8b
Isabel Dec <idec@id.iit.edu>
2017-03-31 12:04:49 -0500

    Created variables and added ‘if’ and ‘else’ functions.

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ boxes[0].addEventListener("click", boxClicked);
 
 
 
+var leftMargin = 0;
+var topMargin = 0;
+var moveAmount = 20;
+
 function boxClicked() {
     console.log("Hey, box clicked!");
+
+    if (leftMargin < 100) {
+        leftMargin += moveAmount;
+    } else {
+        leftMargin -= moveAmount;
+    }
+
+    topMargin += moveAmount;
+
+    boxes[0].style.marginLeft = leftMargin + "px";
+    boxes[0].style.marginTop = topMargin + "px";
 }
 
 
