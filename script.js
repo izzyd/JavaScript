a97d9131cdb380036fd9cbb45414540ba7038acc
Isabel Dec <idec@id.iit.edu>
2017-03-31 11:40:13 -0500

    Created a click eventListener for the first box

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ var boxes = document.querySelectorAll(".box");
 boxes[0].style.backgroundColor = "blue";
 boxes[2].style.backgroundColor = "yellow";
 
+boxes[0].addEventListener("click", boxClicked);
+
+
+
+function boxClicked() {
+    console.log("Hey, box clicked!");
+}
+
+
+
+
 console.log(boxes[0]);
 
 
