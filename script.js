fcedf2a7a7d6dc47666b8a4e0c5b85d5c3cfb3df
Isabel Dec <idec@id.iit.edu>
2017-03-31 11:26:55 -0500

    Grab all .box elements and change box1 to blue and box3 to yellow

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello.");
+var boxes = document.querySelectorAll(".box");
+
+
+boxes[0].style.backgroundColor = "blue";
+boxes[2].style.backgroundColor = "yellow";
+
+console.log(boxes[0]);
+
+
+// alert("Hello.");
